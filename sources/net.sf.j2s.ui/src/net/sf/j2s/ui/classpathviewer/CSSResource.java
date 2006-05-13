462caf47e712f9e40030de4e99c4fd4c2791dea8
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-05-13 19:29:39 +0000

    Fix a bug that the *.css resources' relative path is wrong in the genrated HTML.

--- sources/net.sf.j2s.ui/src/net/sf/j2s/ui/classpathviewer/CSSResource.java
+++ sources/net.sf.j2s.ui/src/net/sf/j2s/ui/classpathviewer/CSSResource.java
@@ -18,6 +18,12 @@ public class CSSResource extends Resource implements IExternalResource {
 				if (idx != -1) {
 					buf.append(path.substring(0, idx + 1));
 				}
+			} else if (p instanceof CompositeResources) {
+				CompositeResources cc = (CompositeResources) p;
+				String path = cc.getBinRelativePath();
+				if (path != null) {
+					buf.append(path);
+				}
 			}
 			buf.append(getRelativePath());
 			
