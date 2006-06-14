ce07317210a724a1425df5d917c7062d51e2f252
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-06-14 09:35:30 +0000

    Implement incremental building without enhancing JDT core jar

--- sources/net.sf.j2s.ui/src/net/sf/j2s/ui/property/J2SPropertyPage.java
+++ sources/net.sf.j2s.ui/src/net/sf/j2s/ui/property/J2SPropertyPage.java
@@ -8,36 +8,22 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
-
+import net.sf.j2s.core.Java2ScriptProjectNature;
 import net.sf.j2s.ui.classpath.Resource;
-import net.sf.j2s.ui.jdtenhancer.EnhancerInfo;
-import net.sf.j2s.ui.jdtenhancer.JarUtil;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.BusyIndicator;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.program.Program;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PropertyPage;
 
 public class J2SPropertyPage extends PropertyPage {
@@ -57,7 +43,7 @@ public class J2SPropertyPage extends PropertyPage {
 	
 	private void updateConfig() {
     	IJavaProject jproject = (IJavaProject) getElement();
-        IProject project = jproject.getProject();
+    	IProject project = jproject.getProject();
 		String prjFolder = project.getLocation().toOSString();
 		File file = new File(prjFolder, ".j2s");
 		Properties props = new Properties();
@@ -210,155 +196,18 @@ public class J2SPropertyPage extends PropertyPage {
 		GridData data = new GridData(GridData.FILL);
 		data.grabExcessHorizontalSpace = true;
 		composite.setLayoutData(data);
-
-//        URL starterURL = Java2ScriptUIPlugin.getDefault().getBundle()
-//				.getEntry("/" + File.separator); //$NON-NLS-1$
-//		String path = "."; //$NON-NLS-1$
-//		try {
-//			path = Platform.asLocalURL(starterURL).getFile();
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
-//		}
-//		path = path.replace('/', File.separatorChar);
-//		final String enhancePath = path;
-		//String base = new File(path).getParentFile().getAbsolutePath();
-		final String base = new File(Platform.getInstallLocation().getURL().getFile(), "plugins").getAbsolutePath();
-		final String coreJarName = JarUtil.getJDTCoreJarName(base);
-		String version = null;
-		if (coreJarName != null) {
-			try {
-				version = JarUtil.getEnhancedJDTCoreVersion(base, coreJarName);
-			} catch (IOException e) {
-			}
-		}
-		if (version == null) {
-			Label label = new Label(composite, SWT.NONE);
-			label.setText("It's detected that your JDT Core is not enhanced yet.");
-			label.setForeground(new Color(null, 255, 0, 0));
-			
-			Label label2 = new Label(composite, SWT.NONE);
-			label2.setText("JDT Core must be enhanced before Java2Script builder can take into effect.");
-
-			Link link = new Link(composite, SWT.NONE);
-			link.setText("<a href=\"http://j2s.sourceforge.net/docs/jdt-core-enhance.html\">Why must JDT Core be enhanced?</a>");
-
-			createLabel(composite, "Maybe this is the first time you install Java2Script plugin. To install Java2Script, do as following:");
-			createLabel(composite, "1. Click the following \"Enhance JDT Core\" button, and then exit Eclipse platform. ");
-			createLabel(composite, "2. Copy the enhanced \"org.eclipse.jdt.core_3.*.*.jar\" file and overwrite original " +
-					"jar file in \"plugins\" folder (You must exit Eclipse first, as Eclipse is locking the jar file).");
-			createLabel(composite, "3. Restart the Eclipse you will get the true Java2Script Builder property page.");
-
-			//new Label(composite, SWT.NONE);
-			Label seperator0 = new Label(composite, SWT.SEPARATOR | SWT.HORIZONTAL);
-			seperator0.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
-			final Button btnEnhance = new Button(composite, SWT.PUSH);
-			if (!EnhancerInfo.isJDTCoreAlreadyEnhanced) {
-				btnEnhance.setText("Enhance JDT Core");
-			} else {
-				btnEnhance.setText("Enhance JDT Core Again");
-			}
-			GridData gdBtn = new GridData(GridData.BEGINNING);
-			gdBtn.widthHint = 216;
-			gdBtn.horizontalIndent = 6;
-			gdBtn.verticalIndent = 8;
-			btnEnhance.setLayoutData(gdBtn);
-			new Label(composite, SWT.NONE);
-			
-			Composite pathComp = new Composite(composite, SWT.NONE);
-			pathComp.setLayout(new GridLayout(2, false));
-			GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-			gd.grabExcessHorizontalSpace = true;
-			pathComp.setLayoutData(gd);
-			
-			final Label label3 = new Label(pathComp, SWT.NONE);
-			label3.setText("Enhanced Jar Location:");
-			final Text textPath = new Text(pathComp, SWT.NONE);
-			textPath.setText(new File(base).getParentFile().getAbsolutePath());
-			textPath.setEditable(false);
-			GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
-			gd2.widthHint = 240;
-			textPath.setLayoutData(gd2);
-			final Button btnOpen = new Button(pathComp, SWT.PUSH);
-			GridData gd3Btn = new GridData(GridData.BEGINNING);
-			gd3Btn.widthHint = 216;
-			gd3Btn.horizontalSpan = 2;
-			//gd3Btn.horizontalIndent = 6;
-			btnOpen.setLayoutData(gd3Btn);
-			btnOpen.setText("Open Location with External Explorer");
-			//btnOpen.setLayoutData(new GridData(GridData.BEGINNING, GridData.BEGINNING, false, false, 2, 1));
-			btnOpen.addSelectionListener(new SelectionAdapter() {
-				public void widgetSelected(SelectionEvent e) {
-					Program.launch(textPath.getText());
-				}
-			});
-			
-			final Label label4 = createLabel(composite, "Before you can overwrite the JDT Core jar, you have to exit Ecipse platform. After replacing with the enhanced jar, you can restart the Eclipse.");
-
-			final Button btnRestart = new Button(composite, SWT.PUSH);
-			btnRestart.setText("Exit Eclipse Now");
-			GridData gd2Btn = new GridData(GridData.BEGINNING);
-			gd2Btn.widthHint = 216;
-			gd2Btn.horizontalIndent = 6;
-			btnRestart.setLayoutData(gd2Btn);
-			//btnRestart.setLayoutData(new GridData(GridData.END, GridData.BEGINNING, true, false));
-			btnRestart.addSelectionListener(new SelectionAdapter() {
-				public void widgetSelected(SelectionEvent e) {
-			        PlatformUI.getWorkbench().close();
-				}
-			});
-			if (!EnhancerInfo.isJDTCoreAlreadyEnhanced) {
-				label3.setEnabled(false);
-				textPath.setEnabled(false);
-				btnOpen.setEnabled(false);
-				label4.setEnabled(false);
-				btnRestart.setEnabled(false);
-			}
-			
-			btnEnhance.addSelectionListener(new SelectionAdapter() {
-				public void widgetSelected(SelectionEvent e) {
-					BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
-						public void run() {
-							try {
-								JarUtil.enhanceJDTCoreTo(coreJarName, base, 
-											new File(base).getParentFile().getAbsolutePath(), base);
-								EnhancerInfo.isJDTCoreAlreadyEnhanced = true;
-								label4.setEnabled(true);
-								btnRestart.setEnabled(true);
-								//btnEnhance.setEnabled(false);
-								label3.setEnabled(true);
-								textPath.setEnabled(true);
-								btnOpen.setEnabled(true);
-								btnEnhance.setText("Enhance JDT Core Again");
-							} catch (IOException e1) {
-								e1.printStackTrace();
-								MessageDialog.openError(getShell(), "Enhance Error", e1.getMessage());
-							}
-						}
-					
-					});
-				}
-			});
-			
-
-			noDefaultAndApplyButton();
-		} else {
-	    	IJavaProject jproject = (IJavaProject) getElement();
-	        IProject project = jproject.getProject();
-			String prjFolder = project.getLocation().toOSString();
-			File file = new File(prjFolder, ".j2s");
-			
-			configPage = new J2SConfigPage(composite, SWT.NONE);
-			configPage.initConfigPage(file);
-			isEnabled = configPage.isCompilerEnabled();
-		}
+    	IJavaProject jproject = (IJavaProject) getElement();
+        IProject project = jproject.getProject();
+		String prjFolder = project.getLocation().toOSString();
+		File file = new File(prjFolder, ".j2s");
+		
+		configPage = new J2SConfigPage(composite, SWT.NONE);
+		configPage.initConfigPage(file);
+		isEnabled = configPage.isCompilerEnabled();
 		return composite;
 	}
 	
 	public boolean performOk() {
-		if (EnhancerInfo.currentDetectedVersion == null) {
-			return true;
-		}
 		updateConfig();
 		if (configPage.isCompilerEnabled()) {
 	        if (getContainer() instanceof PreferenceDialog) {
@@ -371,11 +220,28 @@ public class J2SPropertyPage extends PropertyPage {
 	        IProgressMonitor monitor = null;
 	    	IJavaProject jproject = (IJavaProject) getElement();
 	        IProject project = jproject.getProject();
+	    	try {
+	    		Java2ScriptProjectNature pn = new Java2ScriptProjectNature();
+	    		pn.setProject(project);
+	    		pn.addToBuildSpec("net.sf.j2s.core.java2scriptbuilder");
+	    	} catch (CoreException e) {
+	    		e.printStackTrace();
+	    	}
 	        try {
 				project.build(IncrementalProjectBuilder.CLEAN_BUILD, monitor);
 			} catch (CoreException e) {
 				e.printStackTrace();
 			}
+		} else {
+	    	IJavaProject jproject = (IJavaProject) getElement();
+	    	IProject project = jproject.getProject();
+	    	try {
+	    		Java2ScriptProjectNature pn = new Java2ScriptProjectNature();
+	    		pn.setProject(project);
+	    		pn.removeFromBuildSpec("net.sf.j2s.core.java2scriptbuilder");
+	    	} catch (CoreException e) {
+	    		e.printStackTrace();
+	    	}
 		}
 		return true;
 	}
