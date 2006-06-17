82a4a3da002477dd98180486b6f617dc9aea4822
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-06-17 13:02:40 +0000

    Restructure the SVN repository

---
+++ tests/net.sf.j2s.test.swt/src/net/sf/j2s/test/swt/TestSWTPlugin.java
@@ -0,0 +1,54 @@
+package net.sf.j2s.test.swt;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class TestSWTPlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static TestSWTPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public TestSWTPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static TestSWTPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("net.sf.j2s.test.swt", path);
+	}
+}
