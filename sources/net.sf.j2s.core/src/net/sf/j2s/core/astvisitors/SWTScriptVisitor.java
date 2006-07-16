614eeea33328ae7a704ed17968bfb178e8ba9002
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-07-16 17:09:16 +0000

    Fixed bug of cyclic dependency on generating Clazz.load (...) Filter those checkSubclass, checkParent method on SWT package

--- sources/net.sf.j2s.core/src/net/sf/j2s/core/astvisitors/SWTScriptVisitor.java
+++ sources/net.sf.j2s.core/src/net/sf/j2s/core/astvisitors/SWTScriptVisitor.java
@@ -25,6 +25,7 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Name;
 import org.eclipse.jdt.core.dom.PrefixExpression;
@@ -339,6 +340,56 @@ public class SWTScriptVisitor extends ASTScriptVisitor {
 		super.endVisit(node);
 	}
 
+	protected String[] getFilterMethods() {
+		return new String[] {
+				"org.eclipse.swt.widgets.Widget", "checkSubclass",
+				"org.eclipse.swt.widgets.Dialog", "checkSubclass",
+				"org.eclipse.swt.widgets.Widget", "checkWidget",
+				"org.eclipse.swt.widgets.Display", "checkDevice",
+				"org.eclipse.swt.graphics.Device", "checkDevice"
+		};
+	}
+	/* (non-Javadoc)
+	 * @see net.sf.j2s.core.astvisitors.ASTScriptVisitor#visit(org.eclipse.jdt.core.dom.MethodInvocation)
+	 */
+	public boolean visit(MethodInvocation node) {
+		IMethodBinding methodBinding = node.resolveMethodBinding();
+		String[] filterMethods = getFilterMethods();
+		for (int i = 0; i < filterMethods.length; i += 2) {
+			if (isMethodInvoking(methodBinding, filterMethods[i], filterMethods[i + 1])) {
+				return false;
+			}
+		}
+		return super.visit(node);
+	}
+	/* (non-Javadoc)
+	 * @see net.sf.j2s.core.astvisitors.ASTScriptVisitor#endVisit(org.eclipse.jdt.core.dom.MethodDeclaration)
+	 */
+	public void endVisit(MethodDeclaration node) {
+		IMethodBinding methodBinding = node.resolveBinding();
+		String[] filterMethods = getFilterMethods();
+		for (int i = 0; i < filterMethods.length; i += 2) {
+			if (isMethodInvoking(methodBinding, filterMethods[i], filterMethods[i + 1])) {
+				return ;
+			}
+		}
+		super.endVisit(node);
+	}
+	
+	/* (non-Javadoc)
+	 * @see net.sf.j2s.core.astvisitors.ASTScriptVisitor#visit(org.eclipse.jdt.core.dom.MethodDeclaration)
+	 */
+	public boolean visit(MethodDeclaration node) {
+		IMethodBinding methodBinding = node.resolveBinding();
+		String[] filterMethods = getFilterMethods();
+		for (int i = 0; i < filterMethods.length; i += 2) {
+			if (isMethodInvoking(methodBinding, filterMethods[i], filterMethods[i + 1])) {
+				return false;
+			}
+		}
+		return super.visit(node);
+	}
+	
 	public boolean visit(Block node) {
 		int swtBlockWhileCount = 0;
 		boolean lastSWTBlockWhile = metSWTBlockWhile;
@@ -352,13 +403,15 @@ public class SWTScriptVisitor extends ASTScriptVisitor {
 			if (stmt instanceof ExpressionStatement) {
 				ExpressionStatement expStmt = (ExpressionStatement) stmt;
 				Expression exp = expStmt.getExpression();
-				if (isMethodInvoking(exp, "org.eclipse.swt.widgets.Widget", "checkWidget")) {
-					continue;
+				String[] filterMethods = getFilterMethods();
+				boolean isContinue = false;
+				for (int i = 0; i < filterMethods.length; i += 2) {
+					if (isMethodInvoking(exp, filterMethods[i], filterMethods[i + 1])) {
+						isContinue = true;
+						break;
+					}
 				}
-				if (isMethodInvoking(exp, "org.eclipse.swt.widgets.Display", "checkDevice")) {
-					continue;
-				}
-				if (isMethodInvoking(exp, "org.eclipse.swt.graphics.Device", "checkDevice")) {
+				if (isContinue) {
 					continue;
 				}
 			}
@@ -376,14 +429,43 @@ public class SWTScriptVisitor extends ASTScriptVisitor {
 		//return super.visit(node);
 	}
 	
+	private boolean isMethodInvoking(IMethodBinding methodBinding, String className, String methodName) {
+		if (methodName.equals(methodBinding.getName())) {
+			IMethodBinding findMethodInHierarchy = Bindings.findMethodInHierarchy(methodBinding.getDeclaringClass(), methodName, null);
+			IMethodBinding last = findMethodInHierarchy;
+			int count = 0;
+			while (findMethodInHierarchy != null && (count++) < 10) {
+				last = findMethodInHierarchy;
+				ITypeBinding superclass = last.getDeclaringClass().getSuperclass();
+				if (superclass == null) {
+					break;
+				}
+				findMethodInHierarchy = 
+					Bindings.findMethodInHierarchy(superclass, methodName, null);
+			}
+			if (last == null) {
+				last = methodBinding;
+			}
+			if (className.equals(last.getDeclaringClass().getQualifiedName())) {
+				return true;
+			}
+		}
+		return false;
+	}
 	private boolean isMethodInvoking(Expression exp, String className, String methodName) {
 		if (exp instanceof MethodInvocation) {
 			MethodInvocation method = (MethodInvocation) exp;
+			IMethodBinding methodBinding = method.resolveMethodBinding();
+			if (isMethodInvoking(methodBinding, className, methodName)) {
+				return true;
+			}
+			/*
 			IMethodBinding methodBinding = method.resolveMethodBinding();
 			if (methodName.equals(methodBinding.getName()) &&className.equals(
 					methodBinding.getDeclaringClass().getQualifiedName())) {
 				return true;
 			}
+			*/
 		}
 		return false;
 	}
