cea3b4bc15a709d28fe23998c42c931f5410e89f
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-07-30 12:16:21 +0000

    Code refactor on *Shortcut and *ConfiguartionDelegate

--- trunk/sources/net.sf.j2s.ui/src/net/sf/j2s/ui/launching/J2SApplicationLaunchShortcut.java
+++ trunk/sources/net.sf.j2s.ui/src/net/sf/j2s/ui/launching/J2SApplicationLaunchShortcut.java
@@ -149,23 +149,6 @@ public class J2SApplicationLaunchShortcut implements ILaunchShortcut {
 	protected void launch(IType type, String mode) {
 		ILaunchConfiguration config = findLaunchConfiguration(type, mode);
 		if (config != null) {
-			try {
-				StringBuffer buffer= new StringBuffer(config.getName());
-				buffer.append(DebugUIMessages.DebugUIPlugin_0); //$NON-NLS-1$
-				ILaunchConfigurationWorkingCopy workingCopy= config.copy(buffer.toString());
-				//workingCopy.setAttribute(ATTR_LAUNCHING_CONFIG_HANDLE, config.getMemento());
-				//final ILaunch pendingLaunch= new PendingLaunch(workingCopy, mode, this);
-                
-				DebugPlugin.getDefault().getLaunchManager().addLaunch(new Launch(config, mode, new ISourceLocator() {
-					public Object getSourceElement(IStackFrame stackFrame) {
-						// TODO Auto-generated method stub
-						return null;
-					}
-				}));
-				J2SLaunchingUtil.launchingJ2SApp(config);
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
 			DebugUITools.launch(config, mode);
 		}			
 	}
