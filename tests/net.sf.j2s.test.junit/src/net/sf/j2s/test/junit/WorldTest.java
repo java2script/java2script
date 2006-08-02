9b9d89912fd08e66caff4719d4ba5eb9068da347
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-08-02 12:56:30 +0000

    Add test project for junit implementation

---
+++ tests/net.sf.j2s.test.junit/src/net/sf/j2s/test/junit/WorldTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Java2Script Pacemaker (http://j2s.sourceforge.net)
+ *
+ * Copyright (c) 2006 ognize.com and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ognize.com - initial API and implementation
+ *******************************************************************************/
+
+package net.sf.j2s.test.junit;
+
+import junit.framework.TestCase;
+
+/**
+ * @author josson smith
+ *
+ * 2006-7-21
+ */
+public class WorldTest extends TestCase {
+	public void testWorld() {
+		assertTrue(false);
+	}
+	public static void main(String[] args) {
+		junit.swingui.TestRunner.run(WorldTest.class);
+	}
+}
