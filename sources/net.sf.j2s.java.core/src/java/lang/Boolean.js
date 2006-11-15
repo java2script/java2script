13ad50bae88ca135b66fc9fa4bc9bb067d8a2862
soheil_h_y <soheil_h_y@453f6288-7446-11de-9528-39508ac20fff>
2006-11-15 12:49:12 +0000

    1.parseBoolean is implemented

--- sources/net.sf.j2s.java.core/src/java/lang/Boolean.js
+++ sources/net.sf.j2s.java.core/src/java/lang/Boolean.js
@@ -3,6 +3,14 @@ Boolean.prototype.booleanValue=function(){
 return this.valueOf();
 }
 
+Boolean.prototype.parseBoolean = function(val){
+	if(val == "false" || val == false){
+		return false;
+	}
+	return true;
+}
+
+Boolean.$valueOf = Boolean.parseBoolean = Boolean.prototype.parseBoolean;
 
 Boolean.TRUE=Boolean.prototype.TRUE=new Boolean(true);
 Boolean.FALSE=Boolean.prototype.FALSE=new Boolean(false);
