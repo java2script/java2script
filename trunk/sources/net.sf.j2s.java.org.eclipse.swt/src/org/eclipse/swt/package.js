bdeb87153c153a6d2f9e30960cfadc3b4428b0a1
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-12-02 15:53:01 +0000

    Take another loading try to make sure *.css are not missed.

--- trunk/sources/net.sf.j2s.java.org.eclipse.swt/src/org/eclipse/swt/package.js
+++ trunk/sources/net.sf.j2s.java.org.eclipse.swt/src/org/eclipse/swt/package.js
@@ -1,8 +1,4 @@
 /*
- * The following codes are moved from ClassExt.js, with modifications
- */
-
-/*
  * Standing for SWT CSS, with all "S" into "$". We love dollors. -zhou renjian
  *
  * ATTENTION: Should only be used for SWT only. 
@@ -13,6 +9,124 @@ $WTC$$ = {};
 /* protected */
 $WTC$$.cssAlreadyAggregated = false;
 $WTC$$.cssForcedUsingFile = false;
+
+/* private */
+$WTC$$.timeouts = new Object ();
+$WTC$$.triedTimes = new Object ();
+$WTC$$.cssTestEls = [];
+/* private */
+$WTC$$.trackCSS = function (clazzName) {
+	var el = document.createElement ("DIV");
+	el.style.cssText = "position:absolute;left:-1000px;top:-100px;font-size:0;display:block;";
+	var cssID = $WTC$$.getCSSRuleID (clazzName);
+	el.className = cssID;
+	if (document.body != null) {
+		document.body.appendChild (el);
+	}
+
+	var els = $WTC$$.cssTestEls;
+	for (var i = 0; i < els.length; i++) {
+		if (els[i] == el) {
+			return;
+		}
+	}
+	els[els.length] = el;
+	var f = (function (e, name) {
+		return function () {
+			$WTC$$.removeTesting (name);
+			var tts = $WTC$$.triedTimes;
+			if (tts[e.className] > 1) { // already tried once
+				alert ("[Java2Script] Error in loading CSS for " + name + "!");
+			} else {
+				// alert ("Try to load *.css " + name + " again.");
+				/*
+				 * Remove existed *.css
+				 */
+				var head = document.getElementsByTagName ("HEAD")[0];
+				var links = head.getElementsByTagName ("LINK");
+				for (var i = 0; i < links.length; i++) {
+					if (links[i].id == "c$$." + name) {
+						head.removeChild (links[i]);
+						break;
+					}
+				}
+				$WTC$$.registerCSS (name);
+			}
+		};
+	}) (el, clazzName);
+	var handle = window.setTimeout (f, 15000); // 15 seconds, should be 30s
+	
+	$WTC$$.timeouts[cssID] = handle;
+	var tts = $WTC$$.triedTimes;
+	if (tts[cssID] == null) {
+		tts[cssID] = 1;
+	} else {
+		tts[cssID]++;
+	}
+	if (els.length == 1) { // start global checking
+		$WTC$$.globalChecking ();
+	}
+};
+/* protected */
+$WTC$$.cssLoaded = function (clazzName) {};
+/* protected */
+$WTC$$.globalLoaded = function () {};
+/* private */
+$WTC$$.removeTesting = function (clazzName) {
+	var cssClassName = $WTC$$.getCSSRuleID (clazzName);
+	var els = $WTC$$.cssTestEls;
+	for (var i = 0; i < els.length; i++) {
+		if (els[i].className == cssClassName) {
+			var el = els[i];
+			if (el.parentNode != null) {
+				el.parentNode.removeChild (el);
+			}
+			for (var j = i; j < els.length - 1; j++) {
+				els[j] = els[j + 1];
+			}
+			els.length--;
+			return el;
+		}
+	}
+	return null;
+};
+/* private */
+$WTC$$.globalChecking = function () {
+	var els = $WTC$$.cssTestEls;
+	for (var i = 0; i < els.length; i++) {
+		var el = els[i];
+		var w = Math.max(el.offsetWidth,Math.max(el.clientWidth,el.scrollWidth));
+		if (w == 324) { // there are "width:324px;" rules in SWT's *.css
+			//log ("CSS: " + el.className + " loaded!");
+			$WTC$$.cssLoaded (el.className);
+			window.clearTimeout ($WTC$$.timeouts[el.className]);
+			if (el.parentNode != null) {
+				el.parentNode.removeChild (el);
+			}
+			for (var j = i; j < els.length - 1; j++) {
+				els[j] = els[j + 1];
+			}
+			els.length--;
+		}
+	}
+	if (els.length != 0) {
+		//alert ("continue checking ...");
+		return window.setTimeout ($WTC$$.globalChecking, 100);
+	} else {
+		//log ("finished !");
+		$WTC$$.globalLoaded ();
+	}
+};
+
+/* private */
+$WTC$$.getCSSRuleID = function (clazzName) {
+	var cssRuleID = null;
+	if (clazzName.indexOf ("org.eclipse.swt.") == 0 || clazzName.indexOf ("$wt.")) {
+		var idx = clazzName.indexOf ("wt.") + 3;
+		cssRuleID = "swt." + clazzName.substring (idx);
+	}
+	return cssRuleID.toLowerCase ().replace (/\./g, '-');
+};
 
 /**
  * Register css for the given class. If the given css text is null, it will
@@ -41,6 +155,10 @@ $WTC$$.registerCSS = function (clazzName, cssText) {
 			cssLink.id = cssID;
 			cssLink.href = cssPath;
 			document.getElementsByTagName ("HEAD")[0].appendChild (cssLink);
+			/*
+			 * register onload event callback
+			 */
+			$WTC$$.trackCSS (clazzName);
 		} else {
 			var prefix = "";
 			var idx = cssPath.lastIndexOf ("/");
@@ -95,6 +213,9 @@ $WTC$$.registerCSS = function (clazzName, cssText) {
 			}
 		}
 		
+		/*
+		 * This method will generated a lot non existed *.css resouce requests (404) for server.
+		 */
 		var len = $WTC$$.themes.length;
 		for(var i = 0; i < len; i++){
 			var themeName = $WTC$$.themes[i];
@@ -104,49 +225,13 @@ $WTC$$.registerCSS = function (clazzName, cssText) {
 			cssLink.id = cssID + themeName;
 			cssLink.href = themePath;
 			document.getElementsByTagName ("HEAD")[0].appendChild (cssLink);
+			/*
+			 * OK, theme *.css may be not essential. So no onload callbacks here.
+			 */
 		}
 	}
 };
 
-/*
- * The above codes are moved from ClassExt.js
- */
-
-/*
- * The following codes are moved from ClassLoader.js
- */
- 
-/*
- * loadCSS may be considered part of SWT library. Should be packed with
- * SWT not with Java core.
- *
- * Not used in other *.js yet.
- * - Nov 8, 2006
- */
-/* public */
-$WTC$$.loadCSS = function (cssName) {
-	var cssKey = "";
-	var idx = cssName.lastIndexOf (".");
-	if (idx == -1) {
-		cssKey = cssName + ".css";
-	} else {
-		cssKey = cssName.substring (idx + 1) + ".css";
-	}
-	var resLinks = document.getElementsByTagName ("LINK");
-	for (var i = 0; i < resLinks.length; i++) {
-		var cssPath = resLinks[i].href;
-		if (cssPath.lastIndexOf (cssKey) == cssPath.length - cssKey.length) {
-			return ;
-		}
-	}
-
-	/*-# cssLink -> rel #-*/
-	var cssLink = document.createElement ("LINK");
-	cssLink.rel = "stylesheet";
-	var path = ClazzLoader.getClasspathFor (cssName);
-	cssLink.href = path.substring (0, path.lastIndexOf (".js")) + ".css";
-	document.getElementsByTagName ("HEAD")[0].appendChild (cssLink);
-};
 /*
  * This array will preserves the themes order.
  */
@@ -177,10 +262,6 @@ $WTC$$.registerTheme = function(themeName, themePath){
 	}
 	
 };
-
-/*
- * The above codes are moved from ClassLoader.js
- */
  
 Clazz.declarePackage ("org.eclipse.swt");
 $wt = org.eclipse.swt;
