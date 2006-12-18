16d7dccd5dcdff5bc2b6e9de6443a3a03d1e8393
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2006-12-18 18:04:35 +0000

    Refactor SimpleRPCHttpServlet into a container servlet so that developers have no more need to extend servlet for every SimpleRPCRunnable.

--- trunk/sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/SimpleRPCRequest.java
+++ trunk/sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/SimpleRPCRequest.java
@@ -53,9 +53,76 @@ public class SimpleRPCRequest {
 		}
 	}
 	
+	static String getClassNameURL(SimpleRPCRunnable runnable) {
+		Class oClass = runnable.getClass();
+		String name = oClass.getName();
+		while (name.indexOf('$') != -1) {
+			oClass = oClass.getSuperclass();
+			if (oClass == null) {
+				return null; // should never happen!
+			}
+			name = oClass.getName();
+		}
+		return name;
+	}
+	
+	static SimpleRPCMapping[] mappings = new SimpleRPCMapping[4];
+	
+	public static boolean addMapping(SimpleRPCMapping mapping) {
+		int length = mappings.length;
+		for (int i = 0; i < length; i++) {
+			if (mappings[i] == mapping) {
+				return false;
+			}
+		}
+		for (int i = 0; i < length; i++) {
+			if (mappings[i] == null) {
+				mappings[i] = mapping;
+				return true;
+			}
+		}
+		SimpleRPCMapping[] newMappings = new SimpleRPCMapping[length + 4];
+		for (int i = 0; i < length; i++) {
+			newMappings[i] = mappings[i];
+		}
+		newMappings[length] = mapping;
+		mappings = newMappings;
+		return true;
+	}
+	
+	static String getRunnableURL(final SimpleRPCRunnable runnable) {
+		String clazzNameURL = getClassNameURL(runnable);
+		int length = mappings.length;
+		for (int i = 0; i < length; i++) {
+			if (mappings[i] != null) {
+				String shortURL = mappings[i].getRunnableURL(clazzNameURL);
+				if (shortURL != null) {
+					clazzNameURL = shortURL;
+					break;
+				}
+			}
+		}
+		String url = runnable.baseURL();
+		if (url == null || url.trim().length() == 0) {
+			url = clazzNameURL; 
+		} else {
+			url = url.trim();
+			if (url.charAt(url.length() - 1) != '/') {
+				url += '/';
+			}
+			url += clazzNameURL;
+		}
+		return url;
+	}
+
 	private static void ajaxRequest(final SimpleRPCRunnable runnable) {
 		final HttpRequest request = new HttpRequest();
-		request.open(runnable.method(), runnable.url(), true);
+		String url = getRunnableURL(runnable);
+		String method = runnable.method();
+		if (method == null) {
+			method = "POST";
+		}
+		request.open(method, url, true);
 		request.registerOnReadyStateChange(new XHRCallbackAdapter() {
 			public void onLoaded() {
 				runnable.deserialize(request.getResponseText());
@@ -64,4 +131,5 @@ public class SimpleRPCRequest {
 		});
 		request.send(runnable.serialize());
 	}
+	
 }
