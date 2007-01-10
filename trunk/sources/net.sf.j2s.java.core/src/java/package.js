e940ddac54b858e2fb6bee929af4095bc9307387
jossonsmith <jossonsmith@453f6288-7446-11de-9528-39508ac20fff>
2007-01-10 17:54:42 +0000

    Add lots of existed java.* classes. Just let them be there.

--- trunk/sources/net.sf.j2s.java.core/src/java/package.js
+++ trunk/sources/net.sf.j2s.java.core/src/java/package.js
@@ -33,6 +33,13 @@ window["java.registered"] = false;
 		"$.SubList"
 	]);
 
+	ClazzLoader.jarClasspath (base + "util/Collections.js", [
+		"java.util.Collections",
+		"java.util.Collections.UnmodifiableMap",
+		"java.util.Collections.UnmodifiableMap.UnmodifiableEntrySet",
+		"java.util.Collections.SingletonMap"
+	]);
+
 	ClazzLoader.jarClasspath (base + "lang/reflect.z.js", [
         "java.lang.Void",
 		"$.reflect.AccessibleObject",
