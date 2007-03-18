d438d90d0a88ddc20daaa6e2ca2591922e1dd497
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-03-18 11:22:16 +0000

    Update comments

--- sources/net.sf.j2s.java.core/src/java/lang/ClassExt.js
+++ sources/net.sf.j2s.java.core/src/java/lang/ClassExt.js
@@ -1,15 +1,12 @@
 /******************************************************************************
- * Copyright (c) 2005-2006 ognize.com and others.
+ * Copyright (c) 2007 java2script.org and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Web:
- *     http://j2s.sourceforge.net/
- *     http://sourceforge.net/projects/j2s/
  * Contributors:
- *     ognize.com - initial API and implementation
+ *     Zhou Renjian - initial API and implementation
  *****************************************************************************/
 /*******
  * @author zhou renjian
--- sources/net.sf.j2s.java.core/src/java/lang/Console.js
+++ sources/net.sf.j2s.java.core/src/java/lang/Console.js
@@ -1,7 +1,15 @@
-/*******
- * Copyright ognize.com (2005).
+/******************************************************************************
+ * Copyright (c) 2007 java2script.org and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * @author josson smith
+ * Contributors:
+ *     Zhou Renjian - initial API and implementation
+ *****************************************************************************/
+/*******
+ * @author zhou renjian
  * @create Nov 5, 2005
  *******/
  
