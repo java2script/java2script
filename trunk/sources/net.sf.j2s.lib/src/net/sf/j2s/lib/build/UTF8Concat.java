391efd4ac7ecd078b5a9ff6d3c6f13ec721ed411
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-03-18 11:25:24 +0000

    Update comments

--- trunk/sources/net.sf.j2s.lib/src/net/sf/j2s/lib/build/UTF8Concat.java
+++ trunk/sources/net.sf.j2s.lib/src/net/sf/j2s/lib/build/UTF8Concat.java
@@ -1,14 +1,12 @@
 /*******************************************************************************
- * Java2Script Pacemaker (http://j2s.sourceforge.net)
- *
- * Copyright (c) 2006 ognize.com and others.
+ * Copyright (c) 2007 java2script.org and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     ognize.com - initial API and implementation
+ *     Zhou Renjian - initial API and implementation
  *******************************************************************************/
 
 package net.sf.j2s.lib.build;
@@ -20,7 +18,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * @author josson smith
+ * @author zhou renjian
  *
  * 2006-7-29
  */
