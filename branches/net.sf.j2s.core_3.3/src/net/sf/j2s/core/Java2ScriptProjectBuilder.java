a328dc4f49814bb573f6ee4e26dc10ca00f1467e
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-05-13 02:21:21 +0000

    Branch for Eclipse 3.3

---
+++ branches/net.sf.j2s.core_3.3/src/net/sf/j2s/core/Java2ScriptProjectBuilder.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2007 java2script.org and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Zhou Renjian - initial API and implementation
+ *******************************************************************************/
+
+package net.sf.j2s.core;
+
+import net.sf.j2s.core.builder.JavaBuilder;
+
+/**
+ * @author zhou renjian
+ *
+ * 2006-6-14
+ */
+public class Java2ScriptProjectBuilder extends JavaBuilder {
+	/**
+	 * 
+	 */
+	public Java2ScriptProjectBuilder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
