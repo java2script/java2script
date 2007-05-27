8f59999a3d5386fdf46dff35e320cc092d23caab
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-05-27 15:41:51 +0000

    Setup inner Java2Script hotspot server (Java2Script is going to support Java-like hotspot loading.)

--- branches/net.sf.j2s.core_3.3/src/net/sf/j2s/core/CorePlugin.java
+++ branches/net.sf.j2s.core_3.3/src/net/sf/j2s/core/CorePlugin.java
@@ -1,5 +1,7 @@
 package net.sf.j2s.core;
 
+import net.sf.j2s.core.hotspot.InnerHotspotServer;
+
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
@@ -31,6 +33,9 @@ public class CorePlugin extends Plugin {
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 		plugin = null;
+		if (InnerHotspotServer.isServerStarted()) {
+			InnerHotspotServer.getSingletonServer().stopServer();
+		}
 	}
 
 	/**
