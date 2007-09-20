5e03f0ffc4f632c33553052b7f9c8f63e282e7ce
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-09-20 17:45:14 +0000

    1. Fixed a bug that ShellManager does not destroy (hide) title bar correct in maximized mode 2. Remove Runnability dependency from Clazz, so *.internal.xhtml.* can be used in native HTML applications.

--- sources/net.sf.j2s.java.org.eclipse.swt/src/org/eclipse/swt/internal/xhtml/Clazz.java
+++ sources/net.sf.j2s.java.org.eclipse.swt/src/org/eclipse/swt/internal/xhtml/Clazz.java
@@ -1,10 +1,7 @@
 package org.eclipse.swt.internal.xhtml;
 
-import org.eclipse.swt.internal.RunnableCompatibility;
-
 public class Clazz {
 
 	public native static Runnable makeFunction(Runnable runnable);
-
-	public native static Runnable makeFunction(RunnableCompatibility runnable);
+	
 }
