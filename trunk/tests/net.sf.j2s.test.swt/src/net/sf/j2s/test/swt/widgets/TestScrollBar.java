6a0ce3382d7398fbe06ded41adda2229c8f983d8
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2007-09-24 01:00:42 +0000

    Enable scrollbar for scrollable widget

---
+++ trunk/tests/net.sf.j2s.test.swt/src/net/sf/j2s/test/swt/widgets/TestScrollBar.java
@@ -0,0 +1,107 @@
+package net.sf.j2s.test.swt.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ScrollBar;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Slider;
+
+public class TestScrollBar extends Shell {
+
+	private Composite composite;
+
+	/**
+	 * Launch the application
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		try {
+			Display display = Display.getDefault();
+			TestScrollBar shell = new TestScrollBar(display,
+					SWT.SHELL_TRIM);
+			shell.open();
+			shell.layout();
+			while (!shell.isDisposed()) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the shell
+	 * @param display
+	 * @param style
+	 */
+	public TestScrollBar(Display display, int style) {
+		super(display, style);
+		createContents();
+		setLayout(new FillLayout());
+	}
+
+	/**
+	 * Create contents of the window
+	 */
+	protected void createContents() {
+		setText("SWT Application");
+		setSize(500, 375);
+
+		composite = new Composite(this, SWT.NONE);
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.horizontalSpacing = 0;
+		gridLayout.numColumns = 2;
+		composite.setLayout(gridLayout);
+		composite.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+
+		Composite composite_1 = new Composite(composite, SWT.V_SCROLL);
+		composite_1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		composite_1.setLayout(new GridLayout());
+		
+		composite_1 = new Composite(composite, SWT.H_SCROLL);
+		composite_1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		composite_1.setLayout(new GridLayout());
+		
+		final Composite composite_2 = new Composite(composite, SWT.H_SCROLL | SWT.V_SCROLL);
+		composite_2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		composite_2.setLayout(new GridLayout());
+		
+		composite_2.getVerticalBar().addSelectionListener(new SelectionAdapter() {
+		
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println(composite_2.getVerticalBar().getSelection());
+			}
+		
+		});
+		
+		composite_2.getVerticalBar().setSelection(80);
+		//composite_2.getHorizontalBar().setSelection(80);
+		composite_2.getHorizontalBar().addSelectionListener(new SelectionAdapter() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println(composite_2.getHorizontalBar().getSelection());
+			}
+			
+		});
+		
+		composite_2.getHorizontalBar().setMinimum(20);
+		
+		composite_2.getHorizontalBar().setMaximum(40);
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+}
