ab2cd122fe5d7f259702484ebe4d578c5107a258
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2009-02-08 16:34:50 +0000

    Fix bug that blank SimpleSerializable descendant does not get correct parsing

--- sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/Base64.java
+++ sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/Base64.java
@@ -218,7 +218,8 @@ class Base64 {
         34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
         51, 22, 23, 24, 25
     };
-    
+
+    /*
     public static void main(String args[]) {
         int numRuns  = Integer.parseInt(args[0]);
         int numBytes = Integer.parseInt(args[1]);
@@ -241,4 +242,5 @@ class Base64 {
             }
         }
     }
+    */
 }
