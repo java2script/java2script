d16c3c0df84384cf01db3bddf84096f7072679c2
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2009-04-12 16:53:07 +0000

    Add Simple Store APIs for storing data locally.

---
+++ sources/net.sf.j2s.ajax/store/net/sf/j2s/store/INIFileStore.java
@@ -0,0 +1,88 @@
+package net.sf.j2s.store;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+class INIFileStore implements IStore {
+
+	private File file;
+	
+	private Properties properties;
+	
+	private long lastUpdated;
+	
+	public INIFileStore(String path) {
+		this.file = new File(path);
+		load();
+	}
+	
+	private void load() {
+		properties = new Properties();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(this.file);
+			properties.load(fis);
+		} catch (FileNotFoundException e) {
+			//e.printStackTrace();
+		} catch (IOException e) {
+			//e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					//e.printStackTrace();
+				}
+			}
+		}
+		lastUpdated = new Date().getTime();
+	}
+
+	public String getProperty(String name) {
+		long lastModified = file.lastModified();
+		if (lastModified > lastUpdated) {
+			load();
+		}
+		return properties.getProperty(name);
+	}
+
+	public void setProperty(String name, String value) {
+		long lastModified = file.lastModified();
+		if (lastModified > lastUpdated) {
+			load();
+		}
+		properties.setProperty(name, value);
+		save();
+	}
+
+	private void save() {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(this.file);
+			properties.store(fos, "Java2Script Simple Store");
+		} catch (FileNotFoundException e) {
+			//e.printStackTrace();
+		} catch (IOException e) {
+			//e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					//e.printStackTrace();
+				}
+			}
+		}
+		lastUpdated = new Date().getTime();
+	}
+
+	public boolean isReady() {
+		return true;
+	}
+
+}
---
+++ sources/net.sf.j2s.ajax/store/net/sf/j2s/store/XSSCookieStore.java
@@ -0,0 +1,101 @@
+package net.sf.j2s.store;
+
+/**
+ * 
+ * @author Zhou Renjian (http://zhourenjian.com)
+ *
+ * Mar 28, 2009
+ * 
+ * @j2sSuffix
+ * var ua = navigator.userAgent.toLowerCase ();
+ * var isIE = (ua.indexOf ("msie") != -1);
+ * var isIE6OrEarlier = isIE && ((ua.indexOf ("msie 6.0") != -1)
+ * 		|| (ua.indexOf ("msie 5.5") != -1) || (ua.indexOf ("msie 5.0") != -1));
+ * var xssCookieURL = window["j2s.xss.cookie.url"];
+ * var isLocal = window.location.protocol == "file:";
+ * if (!isLocal && xssCookieURL != null && !isIE6OrEarlier) {
+ * 	net.sf.j2s.store.XSSCookieStore.initialize(xssCookieURL);
+ * }
+ */
+class XSSCookieStore implements IStore {
+
+	private String url;
+
+	private static boolean initialized = false;
+	
+	public XSSCookieStore(String url) {
+		if (url == null) {
+			url = "http://cookie.java2script.org/xss-cookie.html";
+		}
+		this.url = url;
+		initialize(url);
+	}
+	
+	/**
+	 * @j2sNative
+var ua = navigator.userAgent.toLowerCase ();
+var isIE = (ua.indexOf ("msie") != -1);
+if (!isIE) {
+	document.domain = document.domain;
+}
+var xssIfr = document.getElementById ("xss-cookie");
+if (xssIfr != null) {
+	return;
+}
+window.xssCookieReadyCallback = function () {
+	net.sf.j2s.store.XSSCookieStore.initialized = true;
+};
+var xssIfr = document.createElement ("IFRAME");
+xssIfr.id = "xss-cookie";
+xssIfr.src = url;
+xssIfr.style.display = "none";
+document.body.appendChild (xssIfr);
+	 */
+	private static void initialize(String url) {
+		
+	}
+	
+	/**
+	 * @j2sNative
+if (!net.sf.j2s.store.XSSCookieStore.initialized) {
+	return null;
+}
+var xssIfr = document.getElementById ("xss-cookie");
+if (xssIfr == null) {
+	return null;
+}
+try {
+	return xssIfr.contentWindow.readCookie (name);
+} catch (e) {
+	return null;
+}
+	 */
+	public String getProperty(String name) {
+		if (initialized && url != null) {
+			return null;
+		}
+		return null;
+	}
+
+	/**
+	 * @j2sNative
+if (!net.sf.j2s.store.XSSCookieStore.initialized) {
+	return;
+}
+var xssIfr = document.getElementById ("xss-cookie");
+if (xssIfr == null) {
+	return;
+}
+try {
+	xssIfr.contentWindow.createCookie (name, value, 365);
+} catch (e) { }
+	 */
+	public void setProperty(String name, String value) {
+		
+	}
+
+	public boolean isReady() {
+		return initialized;
+	}
+
+}
