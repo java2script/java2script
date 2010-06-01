990ee55721505899aea0297789b903e9d1972b8f
zhourenjian <zhourenjian@453f6288-7446-11de-9528-39508ac20fff>
2010-06-01 07:03:21 +0000

    Synchronize Java2Script*ImageBuilder with JDT 3.5's Builder#newCompiler

--- sources/net.sf.j2s.core/src/net/sf/j2s/core/builder/Java2ScriptBatchImageBuilder.java
+++ sources/net.sf.j2s.core/src/net/sf/j2s/core/builder/Java2ScriptBatchImageBuilder.java
@@ -62,6 +62,9 @@ public class Java2ScriptBatchImageBuilder extends BatchImageBuilder {
 			this,
 			ProblemFactory.getProblemFactory(Locale.getDefault()));
 		CompilerOptions options = newCompiler.options;
+		// temporary code to allow the compiler to revert to a single thread
+		String setting = System.getProperty("jdt.compiler.useSingleThread"); //$NON-NLS-1$
+		newCompiler.useSingleThread = setting != null && setting.equals("true"); //$NON-NLS-1$
 		
 		// enable the compiler reference info support
 		options.produceReferenceInfo = true;
