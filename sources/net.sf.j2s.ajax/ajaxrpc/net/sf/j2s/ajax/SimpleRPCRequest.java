711b3a71d0ea0db7eb63baf4d14ce31752e7d0d7
zhourenjian@gmail.com <zhourenjian@gmail.com@453f6288-7446-11de-9528-39508ac20fff>
2014-01-03 09:46:29 +0000

    Support configure user agent and keep alive headers in Simple RPC

--- sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/SimpleRPCRequest.java
+++ sources/net.sf.j2s.ajax/ajaxrpc/net/sf/j2s/ajax/SimpleRPCRequest.java
@@ -141,6 +141,16 @@ public class SimpleRPCRequest {
 		}
 		
 		final HttpRequest request = getRequest();
+		if (!runnable.supportsKeepAlive()) {
+			request.setRequestHeader("Connection", "close");
+		}
+		if (runnable instanceof ISimpleRequestInfo) {
+			ISimpleRequestInfo reqInfo = (ISimpleRequestInfo) runnable;
+			String ua = reqInfo.getRemoteUserAgent();
+			if (ua != null) {
+				request.setRequestHeader("User-Agent", ua);
+			}
+		}
 		request.open(method, url, true);
 		request.registerOnReadyStateChange(new XHRCallbackAdapter() {
 			public void onLoaded() {
