137a615f3cc2cc5442383c338aef4b658998866a
Udo Borkowski <ub@abego-software.de>
2017-06-28 14:57:55 +0200

    Bug: Wrong expected value when testing result of parsing double literal "1e23"
    In the original "Apache Harmony" test "1e23" resulted in "1.0e23",
    however running the same code in standard JRE (e.g. "1.8.0_121")
    results in "9.999999999999999E22" (the new "expected value").

--- tests/net.sf.j2s.test.junit/src/net/sf/j2s/test/junit/DoubleTest.java
+++ tests/net.sf.j2s.test.junit/src/net/sf/j2s/test/junit/DoubleTest.java
@@ -583,7 +583,7 @@ public class DoubleTest extends junit.framework.TestCase {
 		doTestCompareRawBits("-1.2341234124312332E107", 0xd62ae7a25fe706ecL,
 				"-1.2341234124312331E107");
 
-		doTestCompareRawBits("1e23", 0x44b52d02c7e14af6L, "1.0e23");
+		doTestCompareRawBits("1e23", 0x44b52d02c7e14af6L, "9.999999999999999E22");
 
 		/*
 		 * These particular tests verify that the extreme boundary conditions
