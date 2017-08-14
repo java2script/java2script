ce238da4452b8a6fe20d1c170375d162962ac00b
hansonr <hansonr@stolaf.edu>
2017-08-14 08:48:44 -0400

    test suite work

---
+++ sources/net.sf.j2s.core/test/src/test/Test_Class.java
@@ -0,0 +1,20 @@
+package test;
+
+class Test_Class {
+	
+	Class B() {
+		return null;
+	}
+	
+	static class C extends Test_Class  {
+		
+	}
+
+	Class D() {
+		return C.class;
+	}
+	
+  public static void main(String[] args) {
+  }
+	
+}
---
+++ sources/net.sf.j2s.core/test/src/test/Test_String.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test_String {
+	
+  public static void main(String[] args) {
+	  System.out.println(String.valueOf(true) + "=true");
+	  System.out.println(String.valueOf(1) + "=1");
+	  System.out.println(String.valueOf(1.5) + "=1.5");
+	  System.out.println(String.valueOf(1.5f)+ "=1.5");
+  }
+
+} 
