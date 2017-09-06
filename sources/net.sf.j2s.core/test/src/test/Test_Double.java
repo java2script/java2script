6c535fe3472df8a4429006d623d4280ceca2a071
hansonr <hansonr@stolaf.edu>
2017-09-05 21:57:34 -0500

    test for fixing String + double/float/Double/Float

---
+++ sources/net.sf.j2s.core/test/src/test/Test_Double.java
@@ -0,0 +1,20 @@
+package test;
+
+class Test_Double {
+	
+  public static void main(String[] args) {
+	  System.out.println(("" + new Double(3)).equals("3.0"));
+	  System.out.println(("" + new Float(3)).equals("3.0"));
+	  System.out.println(("" + new Double(3) * 3).equals("9.0"));
+	  float x = 3;
+	  float y = 3;
+	  int i = 3;
+	  System.out.println(("" + x).equals("3.0"));
+	  System.out.println(("" + y).equals("3.0"));
+	  System.out.println(("" + x * y).equals("9.0"));
+	  System.out.println(("" + (y + 0)).equals("3.0"));
+	  System.out.println(("" + (i + 0.0)).equals("3.0"));
+	  System.out.println(("" + Math.round(y)).equals("3"));
+  }
+	
+}
