03e547e3ae2d41d0a8c3fdd4ab3d88c2b90b5f17
hansonr <hansonr@stolaf.edu>
2017-09-05 23:11:32 -0500

    TO int f() { return 'c' } returns string

--- sources/net.sf.j2s.core/test/src/test/Test_Ints.java
+++ sources/net.sf.j2s.core/test/src/test/Test_Ints.java
@@ -3,16 +3,23 @@ package test;
 public class Test_Ints {
 
 	public Test_Ints(float... floats){
-			System.out.println("Test_int(float...) - FAILED");
+			System.out.println("Test_int(float...) - " + (floats[0] != (int) floats[0]));
 		};
 
 	public Test_Ints(int... ints){
-		System.out.println("Test_int(int...) - PASSED");
+		System.out.println("Test_int(int...) - " + (ints.length == 0 || ints[0] == 3));
 	};
 
 	public static void main(String[] args) {
 		new Test_Ints();
 		new Test_Ints(3,5,6);
 		new Test_Ints(3.5f,5.5f,6.5f);
+		System.out.println(getIntFromChar('c') == 99);
+	}
+
+	private static int getIntFromChar(char c) {
+		return c; 
+		
+		FAILS
 	}
 }
