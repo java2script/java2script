cf454e26d37d94a62ae77e2a991c648c2f7538e0
hansonr <hansonr@stolaf.edu>
2017-09-07 09:26:27 -0500

    full primitive casting test.Test_Char.java

---
+++ sources/net.sf.j2s.core/test/src/test/Test_Byte.java
@@ -0,0 +1,14 @@
+package test;
+
+class Test_Byte {
+	
+  public static void main(String[] args) {
+	  byte b = (byte) 0xFF;
+	  short h = (short) 0xFFFF;
+	  int i = (int) 0xFFFFFFFF;
+	 System.out.println(b == -1); 	  
+	 System.out.println(h == -1); 	  
+	 System.out.println(i == -1); 	  
+  }
+	
+}
