7496f772c798fe8e4e8e8f50c539c9253084c10e
hansonr <hansonr@stolaf.edu>
2017-11-13 22:32:13 -0600

    comments

--- sources/net.sf.j2s.java.core/src/swingjs/api/js/JSInterface.java
+++ sources/net.sf.j2s.java.core/src/swingjs/api/js/JSInterface.java
@@ -3,19 +3,19 @@ package swingjs.api.js;
 /** 
  * called by JSmol JavaScript methods using
  * 
- *  this._applet.xxxx()
+ *  this._appletPanel.xxxx$paramTypes()
  *  
  */
 public interface JSInterface {
 
-  int cacheFileByName(String fileName, boolean isAdd);
-  void cachePut(String key, Object data);
-  void destroy();
-  String getFullName();
-  void openFileAsyncSpecial(String fileName, int flags);
-  boolean processMouseEvent(int id, int x, int y, int modifiers, long time, Object jqevent, int scroll);
-  void processTwoPointGesture(float[][][] touches);
-  void setDisplay(HTML5Canvas canvas);
+  int cacheFileByName(String fileName, boolean isAdd);  // $S$Z
+  void cachePut(String key, Object data);               // $S$O
+  void destroy();                                      
+  String getFullName();                             
+  void openFileAsyncSpecial(String fileName, int flags); // $S$I
+  boolean processMouseEvent(int id, int x, int y, int modifiers, long time, Object jqevent, int scroll); // $I$I$I$I$J$O$I
+  void processTwoPointGesture(float[][][] touches); // AFFF
+  void setDisplay(HTML5Canvas canvas);  
   void setScreenDimension(int width, int height);
   boolean setStatusDragDropped(int mode, int x, int y, String fileName);
 	void startHoverWatcher(boolean enable);
