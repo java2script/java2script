c51b80604d05f9836456d803c9df5944e704e1bf
hansonr <hansonr@stolaf.edu>
2018-06-19 10:05:27 +0100

    // BH 6/19/2018 8:49:57 AM fix for checkDeclared

--- sources/net.sf.j2s.java.core/srcjs/js/j2sClazz.js
+++ sources/net.sf.j2s.java.core/srcjs/js/j2sClazz.js
@@ -7,6 +7,7 @@
 
 // Google closure compiler cannot handle Clazz.new or Clazz.super
 
+// BH 6/19/2018 8:49:57 AM fix for checkDeclared
 // BH 5/19/2018 8:22:25 PM fix for new int[] {'a'}
 // BH 4/16/2018 6:14:10 PM msie flag in monitor
 // BH 2/22/2018 12:34:07 AM array.clone() fix
@@ -775,11 +776,10 @@ var _declared = {};
 
 var checkDeclared = function(name, type) {
   if (J2S._debugName && name.toLowerCase() == J2S._debugName)doDebugger();
-  if (_declared[name] == type) {
+  if (_declared[name] != null && _declared[name] == type) {
     var s = (type === 0 ? "interface" : "class") +" " + name + " is defined twice. A prior core file has probably needed to load a class that is in the current core file. Check to make sure that package.js declares the first class read in jarClassPath or that BuildCompress has included all necessary files."
     System.out.println(s);
-    if (J2S._debugCore)
-      doDebugger();
+//    if (J2S._debugCore)    doDebugger();
     }
   _declared[name] = type;
 }
