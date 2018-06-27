f5aa55f7b1a5f281c163a2ee6516e770edd8e3fc
hansonr <hansonr@stolaf.edu>
2018-06-27 18:31:55 +0100

    adds DND onto JDesktop

--- sources/net.sf.j2s.java.core/src/swingjs/plaf/JSDesktopPaneUI.java
+++ sources/net.sf.j2s.java.core/src/swingjs/plaf/JSDesktopPaneUI.java
@@ -1,5 +1,19 @@
 package swingjs.plaf;
 
+import swingjs.JSUtil;
+import swingjs.api.js.DOMNode;
+
 public class JSDesktopPaneUI extends JSPanelUI {
+
+	@Override
+	public DOMNode updateDOMNode() {
+	    boolean isNew = (domNode == null);
+		super.updateDOMNode();
+		if (isNew) {
+			$(domNode).addClass("swingjs-desktop");
+			JSUtil.J2S._setDragDropTarget(domNode);
+		}		
+		return domNode;
+	}
 
 }
