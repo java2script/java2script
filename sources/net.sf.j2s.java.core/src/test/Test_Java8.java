b1af5b235e7f230b0f148c3ed70849d47d739efc
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2018-08-05 23:17:04 -0500

    adds Clazz.newLambda for efficient lambda::methods and lambda::new
    fixes lambda::new
    
    Note that Function and Consumer are minimal classes. They do not have a
    getClass() method, for instance.

--- sources/net.sf.j2s.java.core/src/test/Test_Java8.java
+++ sources/net.sf.j2s.java.core/src/test/Test_Java8.java
@@ -11,12 +11,21 @@ import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.IntConsumer;
 
 import test.baeldung.doublecolon.Computer;
 import test.baeldung.doublecolon.MacbookPro;
 
 public class Test_Java8 extends Test_ implements PropertyChangeListener { 
 
+	public Test_Java8() {
+		System.out.println("null constructor");
+	}
+	
+	public Test_Java8(String s) {
+		System.out.println("new Test_Java8(" + s + ")");
+	}
+	
 	public static void test() {
 		
 	}
@@ -24,11 +33,23 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 	enum E {
 		E1, E2;
 	}
-	int test1 = 1; 
+	int test1 = 1;  
 	
     Object test2(Object o) {
 		System.out.println("test1 is " + test1 + " " + o);
 		return o;
+	}; 
+
+	static int test1s = 2;
+	
+    static Integer test2is(Integer i) {
+		System.out.println("test1s is " + test1s + " " + i);
+		return i;
+	};
+
+    Integer test2i(Integer i) {
+		System.out.println("test1 is " + test1 + " " + i);
+		return i;
 	};
 
 	public static boolean isMoreThanFifty(int n1, int n2) {
@@ -45,9 +66,6 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 		return newList; 
 	}
 
-	public Test_Java8() {System.out.println("null constructor");}
-	public Test_Java8(Object o) {System.out.println("o is " + o);}
-	
 	@FunctionalInterface
 	public interface ITest {
 	    Test_Java8 c(Object o);
@@ -56,40 +74,10 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 	public static void main(String[] args) {
 		
 	    Comparator comp = Comparator.naturalOrder();
-		
-		
-		System.out.println("E.E1 is " + E.E1);
-		
-		final String function = "" + new Date();
-		
-		int[][][] x = new int[5][][];
-		Function<Integer, int[][][]> ifi = new Function<Integer, int[][][]>() {
-			
-			@Override
-			public int[][][] apply(Integer t) {
-				System.out.println(function);
-				return new int[t.intValue()][][]; 
-			}
-		};
-		int[][][] a = ifi.apply(5); 
-		
-	    a = (new Function<Integer, int[][][]>() {
-			@Override
-			public int[][][] apply(Integer t) {
-				return new int[t.intValue()][][];
-			}
-		}).apply(5); 
 
-	    Function<Integer, int[][][]> iaCreator = int[][][]::new;
-		a = iaCreator.apply(5); 
-		System.out.println("a.length is " + a.length);
-		Test_Java8[] atest = new Test_Java8[5];
-		// following format only works for 
-		Function<Object,Test_Java8> iaCreator2= Test_Java8::new;
+	    new Test_Java8().propertyChange(null); // checking for nonqualified
 		
-		ITest t;
-		t = Test_Java8::new;
-		t = new ITest() {
+		ITest t = new ITest() {
 			@Override
 			public Test_Java8 c(Object o) {
 				return new Test_Java8(o + "??");
@@ -98,22 +86,94 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 		Test_Java8 test = t.c("new");
 				
 		System.out.println("what is test? " + test);
-		Consumer<String> c = s -> System.out.println(s);
-		c = System.out::println; 
-		c.accept("testing");
+		
+		
+		
+		System.out.println("E.E1 is " + E.E1);
+		
 
 		
+		final String function = "" + new Date();
 		
-		Function<Consumer<String>, ?> f = new Test_Java8(null)::test2;
-		// Q: where is the object in this expression? test.test2.apply(test, [args])
-		f.apply(c); 
+		
+// Function
+		
+		int[][][] a; 
+
+		Function<Integer, int[][][]> ifi = new Function<Integer, int[][][]>() {
+			
+			@Override
+			public int[][][] apply(Integer t) {
+				System.out.println(function);
+				return new int[t.intValue()][][]; 
+			}
+		};
+		a = ifi.apply(5); 
+		System.out.println("a length is " + a.length);
+		
+	    a = (new Function<Integer, int[][][]>() {
+			@Override
+			public int[][][] apply(Integer t) {
+				return new int[t.intValue()][][];
+			}
+		}).apply(6); 
+		System.out.println("a.length is " + a.length); 
+		
+//	    // Lambda_C
+	    
+	    Function<Integer, int[][][]> iaCreator = int[][][]::new;
+		a = iaCreator.apply(7);
+		System.out.println("a.length is " + a.length);
+
+		Function<String,Test_Java8> iaCreator2= Test_Java8::new;
+		iaCreator2.apply("testNew");
+		
+		
+		Consumer<String> c = s -> System.out.println(s);
+		c.accept("testingPointer");
+
+		c = System.out::println; 
+		c.accept("testingDoubleColon");
+
+		Function<Integer, Integer> f1 = new Test_Java8("for f1")::test2i;
+		f1.apply(new Integer(3));
+
+		Function<Integer, Integer> f2 = Test_Java8::test2is;
+		f2.apply(new Integer(3));
+
+		Function<Consumer<String>, ?> f = new Test_Java8("for f")::test2;		
+		f.apply(c);
 
 		Test_Java8 t8 = new Test_Java8("new8");
 		f = t8::test2;
 		f.apply(c);
+		f.apply(System.out::println);
+
+		// lambda_M
 		
-		t8.propertyChange(null); // checking for nonqualified
-				
+		Computer c1 = new Computer(2015, "white", 100);
+		Computer c2 = new MacbookPro(2009, "black", 100);
+		List<Computer> inventory = Arrays.asList(c1, c2);
+		
+		inventory.forEach(System.out::println); // works because System.out.println(Object) exists
+		
+		// note that YOU CANNOT HAVE BOTH  static Computer.turnOnPc(Object) and computer.turnOnPc()
+		// so it should be sufficient to call one or the other:
+		// Computer.turnOnPc$Computer.apply(x)
+		// or 
+		// Computer.prototype.turnOnPc$.apply()
+		
+		inventory.forEach(Computer::turnOnPc); // works because c.turnOnPc() exists
+		inventory.forEach(Computer::turnOffPcStatic); // works because Computer.turnOffPcStatic(Computer) exists
+
+
+		
+		// x -> y()  lambda_E
+
+		Function<Integer, String> f3 = i -> Integer.toHexString(i);
+		System.out.println("testing->"+ f3.apply(new Integer(300)));
+
+
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(Integer.valueOf(30));
 		list.add(Integer.valueOf(50));
@@ -121,15 +181,6 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 		BiPredicate<Integer, Integer> bp = (i1, i2) -> i1.intValue() > 3;
 
 		System.out.println(findNumbers(list, bp).size());
-
-		long j = 1;
-		int i = 3;
-		j &= ~(1L << i);
-		j = 1L << i;
-		j = 1L << 3;
-		j &= 1L << i;
-		j = ~i;
-		j = ~(1L << i);
 
 		System.out.println("=== RunnableTest ===");
 
@@ -150,12 +201,6 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 		r2.run();
 
 		
-		Computer c1 = new Computer(2015, "white", 100);
-		Computer c2 = new MacbookPro(2009, "black", 100);
-		List<Computer> inventory = Arrays.asList(c1, c2);
-		inventory.forEach(System.out::println);
-		inventory.forEach(Computer::turnOnPc);
-		inventory.forEach(Computer::turnOffPcStatic);
 		System.out.println("Test_Java8 OK");
 	}
 	
