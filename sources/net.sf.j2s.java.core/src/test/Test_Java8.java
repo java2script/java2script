e00e0638cfddf83097743b5879961ce91c24ce6d
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2018-08-11 22:48:20 -0500

    J2S 3.2.2.03 all stream tests working
    performance could be better.
    
    Test_J8_Speed map with 40,000 4-element ArrayLists flattened to one list
    with 160,000 elements using:
    
    map.values().stream().flatMap(c ->
    c.stream()).collect(Collectors.toList())
    
           stream      for-loop
    Chrome  182 ms      174 ms
    FireFox 728 ms      396 ms
    Edge    366 ms      186 ms
    
    Java     14 ms       14 ms
    
    
    So there is no real advantage without parallelism, I think. Still, it is
    important to cover this.
    
    - arity worked out
    - static vs. nonstatic calling in functional interfaces worked out

--- sources/net.sf.j2s.java.core/src/test/Test_Java8.java
+++ sources/net.sf.j2s.java.core/src/test/Test_Java8.java
@@ -7,17 +7,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.IntConsumer;
+import java.util.function.Predicate;
 
 import test.baeldung.doublecolon.Computer;
 import test.baeldung.doublecolon.MacbookPro;
 
+
 public class Test_Java8 extends Test_ implements PropertyChangeListener { 
 
+	
 	public Test_Java8() {
 		System.out.println("null constructor");
 	}
@@ -34,7 +35,52 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 		E1, E2;
 	}
 	int test1 = 1;  
+
+	void testP1() {
+    	class MyPredicate implements Predicate<String> {
+
+			@Override
+			public boolean test(String t) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    		
+			MyPredicate(String s) {
+				
+			}
+    	}
+    	MyPredicate x = new MyPredicate("xx");
+    	Consumer<String> c = MyPredicate::new;
+    	
+    	class MyPredicateA implements Predicate<String> {
+
+			@Override
+			public boolean test(String t) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    		
+    	}
+
+	}
 	
+	void testP2() {
+    	class MyPredicate implements Predicate<String> {
+
+			@Override
+			public boolean test(String t) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    		
+			MyPredicate(String s) {
+				
+			}
+    	}
+    	MyPredicate x = new MyPredicate("xx");
+    	Consumer<String> c = MyPredicate::new;
+	}
+
     Object test2(Object o) {
 		System.out.println("test1 is " + test1 + " " + o);
 		return o;
@@ -68,140 +114,196 @@ public class Test_Java8 extends Test_ implements PropertyChangeListener {
 
 	@FunctionalInterface
 	public interface ITest {
-	    Test_Java8 c(Object o);
+	    Test_Java8 c(Object o); 
 	}
 	 
-	public static void main(String[] args) {
-		
-	    Comparator comp = Comparator.naturalOrder();
+    public  Predicate<String> makeRef(Predicate<String> predicate) {
+    	class MyPredicate implements Predicate<String> {
 
-	    new Test_Java8().propertyChange(null); // checking for nonqualified
-		
-		ITest t = new ITest() {
 			@Override
-			public Test_Java8 c(Object o) {
-				return new Test_Java8(o + "??");
-			}			
-		};
-		Test_Java8 test = t.c("new");
+			public boolean test(String t) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    		
+			MyPredicate(String s) {
 				
-		System.out.println("what is test? " + test);
-		
-		
-		
-		System.out.println("E.E1 is " + E.E1);
-		
-
-		
-		final String function = "" + new Date();
-		
-		
-// Function
-		
-		int[][][] a; 
-
-		Function<Integer, int[][][]> ifi = new Function<Integer, int[][][]>() {
-			
-			@Override
-			public int[][][] apply(Integer t) {
-				System.out.println(function);
-				return new int[t.intValue()][][]; 
 			}
-		};
-		a = ifi.apply(5); 
-		System.out.println("a length is " + a.length);
-		
-	    a = (new Function<Integer, int[][][]>() {
-			@Override
-			public int[][][] apply(Integer t) {
-				return new int[t.intValue()][][];
-			}
-		}).apply(6); 
-		System.out.println("a.length is " + a.length); 
-		
-//	    // Lambda_C
-	    
-	    Function<Integer, int[][][]> iaCreator = int[][][]::new;
-		a = iaCreator.apply(7);
-		System.out.println("a.length is " + a.length);
+    	}
+    	MyPredicate x = new MyPredicate("xx");
+    	Consumer<String> c = MyPredicate::new;
+    	return x;
+    }
+
+
+    public static void main(String[] args) {
+
 
 		Function<String,Test_Java8> iaCreator2= Test_Java8::new;
 		iaCreator2.apply("testNew");
 		
-		
-		Consumer<String> c = s -> System.out.println(s);
-		c.accept("testingPointer");
-
-		c = System.out::println; 
-		c.accept("testingDoubleColon");
-
-		Function<Integer, Integer> f1 = new Test_Java8("for f1")::test2i;
-		f1.apply(new Integer(3));
-
-		Function<Integer, Integer> f2 = Test_Java8::test2is;
-		f2.apply(new Integer(3));
-
-		Function<Consumer<String>, ?> f = new Test_Java8("for f")::test2;		
-		f.apply(c);
-
-		Test_Java8 t8 = new Test_Java8("new8");
-		f = t8::test2;
-		f.apply(c);
-		f.apply(System.out::println);
-
-		// lambda_M
-		
-		Computer c1 = new Computer(2015, "white", 100);
-		Computer c2 = new MacbookPro(2009, "black", 100);
-		List<Computer> inventory = Arrays.asList(c1, c2);
-		
-		inventory.forEach(System.out::println); // works because System.out.println(Object) exists
-		
-		// note that YOU CANNOT HAVE BOTH  static Computer.turnOnPc(Object) and computer.turnOnPc()
-		// so it should be sufficient to call one or the other:
-		// Computer.turnOnPc$Computer.apply(x)
-		// or 
-		// Computer.prototype.turnOnPc$.apply()
-		
-		inventory.forEach(Computer::turnOnPc); // works because c.turnOnPc() exists
-		inventory.forEach(Computer::turnOffPcStatic); // works because Computer.turnOffPcStatic(Computer) exists
-
-
-		
-		// x -> y()  lambda_E
-
-		Function<Integer, String> f3 = i -> Integer.toHexString(i);
-		System.out.println("testing->"+ f3.apply(new Integer(300)));
-
-
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(Integer.valueOf(30));
-		list.add(Integer.valueOf(50));
-
-		BiPredicate<Integer, Integer> bp = (i1, i2) -> i1.intValue() > 3;
-
-		System.out.println(findNumbers(list, bp).size());
-
-		System.out.println("=== RunnableTest ===");
-
-		// Anonymous Runnable 
-		Runnable r1 = new Runnable() {
-
-			@Override
-			public void run() {
-				System.out.println("Hello world one!");
-			}
-		};
-
-		// Lambda Runnable
-		Runnable r2 = () -> System.out.println("Hello world two!");
-
-		// Run em!
-		r1.run();
-		r2.run();
-
-		
-		System.out.println("Test_Java8 OK");
+//
+//		Comparator comp = Comparator.naturalOrder();
+//
+//	    new Test_Java8().propertyChange(null); // checking for nonqualified
+//		
+//		ITest t = new ITest() {
+//			@Override
+//			public Test_Java8 c(Object o) {
+//				return new Test_Java8(o + "??");
+//			}			
+//		}; 
+//		Test_Java8 test = t.c("new");
+//				
+//		System.out.println("what is test? " + test);
+//		
+//	   	
+//		
+//		System.out.println("E.E1 is " + E.E1);
+//		
+//
+//		
+//		final String function = "" + new Date();
+//		
+//		
+//// Function
+//	
+//		int[][][] a; 
+//
+//		Function<Integer, int[][][]> ifi = new Function<Integer, int[][][]>() {
+//			
+//			@Override
+//			public int[][][] apply(Integer t) {
+//				System.out.println(function);
+//				return new int[t.intValue()][][]; 
+//			}
+//		};
+//		a = ifi.apply(5); 
+//		System.out.println("a length is " + a.length);
+//		
+//	    a = (new Function<Integer, int[][][]>() {
+//			@Override
+//			public int[][][] apply(Integer t) {
+//				return new int[t.intValue()][][];
+//			}
+//		}).apply(6); 
+//		System.out.println("a.length is " + a.length); 
+//		
+////	    // Lambda_C
+//	   
+//	    Function<Integer, int[][][]> iaCreator = int[][][]::new;
+//		a = iaCreator.apply(7);
+//		System.out.println("a.length is " + a.length);
+//
+//		
+//		Consumer<String> c = s -> System.out.println(s);
+//		c.accept("testingPointer");
+//
+//		c = System.out::println; 
+//		c.accept("testingDoubleColon");
+//
+//		Function<Integer, Integer> f1 = new Test_Java8("for f1")::test2i;
+//		f1.apply(new Integer(3));
+//
+//		Function<Integer, Integer> f2 = Test_Java8::test2is;
+//		f2.apply(new Integer(3));
+//
+//		Function<Consumer<String>, ?> f = new Test_Java8("for f")::test2;		
+//		f.apply(c);
+//
+//		
+//		
+//		
+//		Test_Java8 t8 = new Test_Java8("new8");
+//		f = t8::test2;
+//		f.apply(c);
+//		f.apply(System.out::println);
+//
+//		// lambda_M
+//		
+//		Computer c1 = new Computer(2015, "white", 100);
+//		Computer c2 = new MacbookPro(2009, "black", 100);
+//		List<Computer> inventory = Arrays.asList(c1, c2);
+//		
+//		inventory.forEach(System.out::println); // works because System.out.println(Object) exists
+//		
+//		// note that YOU CANNOT HAVE BOTH  static Computer.turnOnPc(Object) and computer.turnOnPc()
+//		// so it should be sufficient to call one or the other:
+//		// Computer.turnOnPc$Computer.apply(x)
+//		// or 
+//		// Computer.prototype.turnOnPc$.apply()
+//		
+//		inventory.forEach(Computer::turnOnPc); // works because c.turnOnPc() exists
+//		inventory.forEach(Computer::turnOffPcStatic); // works because Computer.turnOffPcStatic(Computer) exists
+//
+//
+//		
+//		// x -> y()  lambda_E
+//
+//		Function<Integer, String> f3 = i -> Integer.toHexString(i);
+//		System.out.println("testing->"+ f3.apply(new Integer(300)));
+//
+//
+//		
+//		
+//		List<Integer> list = new ArrayList<Integer>();
+//		list.add(Integer.valueOf(30));
+//		list.add(Integer.valueOf(50));
+//
+//
+//		
+//		int i3 = 3;
+//
+//		BiPredicate<Integer, Integer> bp = (i1, i2) -> i1.intValue() > i3;
+//
+//		Predicate<Integer> bpi = i1 -> i1 > i3;
+//		
+//		int i = 3;
+//		
+//		//list.stream().allMatch(i -> i > i3);
+//
+//		list.stream().allMatch(bpi);
+//
+//		Runnable x = new Runnable() {
+//
+//			@Override
+//			public void run() {
+//
+//				System.out.println("i is " + i);
+//				
+//				BiPredicate<Integer, Integer> bp = (i1, i2) -> i1.intValue() + i2.intValue() > i3;				
+//
+//				list.stream().allMatch(i -> i > i3);
+//
+//			}
+//			
+//			
+//		};
+//
+//		x.run();
+//		
+//		System.out.println(findNumbers(list, bp).size());
+//
+//		System.out.println("=== RunnableTest ===");
+//
+//		// Anonymous Runnable 
+//		Runnable r1 = new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				System.out.println("Hello world one!");
+//			}
+//		};
+//
+//		// Lambda Runnable
+//		Runnable r2 = () -> System.out.println("Hello world two!");
+//
+//		// Run em!
+//		r1.run();
+//		r2.run();
+//
+//		
+//		System.out.println("Test_Java8 OK");
 	}
 	
 	
