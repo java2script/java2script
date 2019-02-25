cc61ebd0ed137d72add7328a1797e5c3a8322681
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-02-25 06:03:21 -0600

    cleanup

--- sources/net.sf.j2s.java.core/src/swingjs/a2s/Util.java
+++ sources/net.sf.j2s.java.core/src/swingjs/a2s/Util.java
@@ -1,11 +1,20 @@
 package swingjs.a2s;
 
-import java.awt.Graphics;
-
+//import java.awt.Color;
+//import java.awt.Component;
+//import java.awt.Font;
 
 public class Util {
-
-	public static void drawString(Graphics g, String text, int x, int y) {
-		g.drawString(text,  x,  y);
-	}
+//	public int junk =1;
+//	static Color CONTROL = new Color(238,238,238); // #EEEEEE
+//	static Color CONTROL_TEXT = new Color(51,51,51); // #333333
+//	static void setAWTWindowDefaults(Component c) {
+//		c.setBackground(CONTROL);
+//		c.setForeground(CONTROL_TEXT);
+//		c.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
+//	}
+//	
+//	public static void drawString(Graphics g, String text, int x, int y) {
+//		g.drawString(text,  x,  y);
+//	}
 }
