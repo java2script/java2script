1e93f7c578e03dd07f81cbaec5e3be88b1f31a8d
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-03-07 21:31:08 -0600

    AWT Frame/Dialog defaults

--- sources/net.sf.j2s.java.core/src/swingjs/a2s/Util.java
+++ sources/net.sf.j2s.java.core/src/swingjs/a2s/Util.java
@@ -1,18 +1,18 @@
 package swingjs.a2s;
 
-//import java.awt.Color;
-//import java.awt.Component;
-//import java.awt.Font;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 
 public class Util {
 //	public int junk =1;
 //	static Color CONTROL = new Color(238,238,238); // #EEEEEE
 //	static Color CONTROL_TEXT = new Color(51,51,51); // #333333
-//	static void setAWTWindowDefaults(Component c) {
-//		c.setBackground(CONTROL);
-//		c.setForeground(CONTROL_TEXT);
-//		c.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
-//	}
+	static void setAWTWindowDefaults(Component c) {
+		c.setBackground(Color.white);
+		c.setForeground(Color.black);
+		c.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
+	}
 //	
 //	public static void drawString(Graphics g, String text, int x, int y) {
 //		g.drawString(text,  x,  y);
