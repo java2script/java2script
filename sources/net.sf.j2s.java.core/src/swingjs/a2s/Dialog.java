c8186177c8611bfb8832d59d07ec7da582014f6e
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-03-07 21:29:06 -0600

    AWT Dialog default font/background/color

--- sources/net.sf.j2s.java.core/src/swingjs/a2s/Dialog.java
+++ sources/net.sf.j2s.java.core/src/swingjs/a2s/Dialog.java
@@ -16,7 +16,7 @@ public class Dialog extends JDialog implements A2SContainer {
 		listener = new A2SListener();
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
-		//Util.setAWTWindowDefaults(this);
+		Util.setAWTWindowDefaults(this);
 	}
 
 	public Dialog(Frame owner) {
