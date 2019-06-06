acfa7eeaf172f74c1e99e8679401dce12bd21e78
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-06-06 16:06:59 -0500

    J2SObjectInterface adds functions for JSmol
    	Object doAjax(Object url, String postOut, Object bytesOrStringOut,
    boolean isBinary);
    
    		Object newGrayScaleImage(Object context, Object image, int width, int
    height, int[] grayBuffer);
    
    	void showInfo(JSAppletObject html5Applet, boolean show);
    
    	void clearConsole(JSAppletObject html5Applet);
    
    	boolean isBinaryUrl(String filename);
    
    	void saveImage(JSAppletObject html5Applet, String type, String
    fileName);
    
    	void repaint(JSAppletObject html5Applet, boolean asNewThread);
    
    	void setCanvasImage(Object canvas, int width, int height);
    
    	Object getHiddenCanvas(JSAppletObject html5Applet, String string, int
    w, int h);
    
    	Object loadFileAsynchronously(Object fileLoadThread, JSAppletObject
    html5Applet, String fileName, Object appData);
    
    	void resizeApplet(Object html5Applet, int[] dims);

--- sources/net.sf.j2s.java.core/src/javajs/api/js/J2SObjectInterface.java
+++ sources/net.sf.j2s.java.core/src/javajs/api/js/J2SObjectInterface.java
@@ -1,12 +1,34 @@
 package javajs.api.js;
 
 /**
- * methods in JSmol JavaScript accessed in Jmol
+ * methods in j2s JavaScript accessed in Jmol
  */
 public interface J2SObjectInterface {
 
 	Object doAjax(String url, String postOut, Object bytesOrStringOut, Object info);
 
+	Object doAjax(Object url, String postOut, Object bytesOrStringOut, boolean isBinary);
+
 	void applyFunc(Object func, Object data);
+
+	Object newGrayScaleImage(Object context, Object image, int width, int height, int[] grayBuffer);
+
+	void showInfo(JSAppletObject html5Applet, boolean show);
+
+	void clearConsole(JSAppletObject html5Applet);
+
+	boolean isBinaryUrl(String filename);
+
+	void saveImage(JSAppletObject html5Applet, String type, String fileName);
+
+	void repaint(JSAppletObject html5Applet, boolean asNewThread);
+
+	void setCanvasImage(Object canvas, int width, int height);
+
+	Object getHiddenCanvas(JSAppletObject html5Applet, String string, int w, int h);
+
+	Object loadFileAsynchronously(Object fileLoadThread, JSAppletObject html5Applet, String fileName, Object appData);
+
+	void resizeApplet(Object html5Applet, int[] dims);
 
 }
