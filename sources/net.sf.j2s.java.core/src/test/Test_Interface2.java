3747862694d9a14382035507123ac19d8d5c9895
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-07-13 13:33:30 +0100

    interface testing -- transpiler does allow static methods in interfaces

--- sources/net.sf.j2s.java.core/src/test/Test_Interface2.java
+++ sources/net.sf.j2s.java.core/src/test/Test_Interface2.java
@@ -8,6 +8,7 @@ public class Test_Interface2 implements Test_Interface {
 	 ClassLoader.getSystemClassLoader().setClassAssertionStatus("test.Test_Interface", true);
 	 System.out.println("in the interface " + x + " " + y + " " + s.length);
 	 assert("5 y".equals(x + " " + y));
+	 assert(Test_Interface3.doTest(3) == 4);
 	 System.out.println("Test_Interface OK");
 	 
   }
