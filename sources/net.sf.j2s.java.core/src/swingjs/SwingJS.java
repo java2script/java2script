08e3ec1e32f7363d38b6b82dc0e885f95dfa385e
hansonr <hansonr@STO24954W.ad.stolaf.edu>
2019-10-20 14:29:58 -0500

    A placeholder for window.SwingJS

---
+++ sources/net.sf.j2s.java.core/src/swingjs/SwingJS.java
@@ -0,0 +1,13 @@
+package swingjs;
+
+/**
+ * Abstract class for the SwingJS object -- only called by reference; set to self.SwingJS in JSToolkit
+ * 
+ * @author hansonr
+ *
+ */
+public abstract class SwingJS {
+
+	public int eventID = 0;
+
+}
