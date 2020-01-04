d27ae0a29eae7e3aa362cedbbfb03583c0c524e7
BobHanson <hansonr@stolaf.edu>
2020-01-03 22:17:34 -0600

    test for @j2sAlias issue

--- sources/net.sf.j2s.java.core/src/test/Test_ClassBase.java
+++ sources/net.sf.j2s.java.core/src/test/Test_ClassBase.java
@@ -3,8 +3,42 @@ package test;
 @SuppressWarnings("rawtypes")
 class Test_ClassBase extends Test_ {
 
+	public static class PTest extends PTest0<Integer> {
+
+		@Override
+		public Integer add(String s, Integer n) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Integer remove(String s, Integer n) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+	public static abstract class PTest0<N extends Number> implements PTestI<N, String> {
+
+		@Override
+		public N remove(String s, N n) {
+			// TODO Auto-generated method stub
+			return add(s, n);
+		}
+
+
+	}
+
+	public interface PTestI<P, T> {
+		public P add(T t, P p);
+		public P remove(T t, P p);
+
+	}
+
 	static class Parser extends BaseParser{
 	
+		@Override
 		void test() {
 			System.out.println("Parser.test");
 		}
@@ -25,6 +59,8 @@ class Test_ClassBase extends Test_ {
 	public static void main(String[] args) {
 
 		new Test_ClassBase.Parser().doTest();
+		
+		new PTest();
 		
 	}
 
