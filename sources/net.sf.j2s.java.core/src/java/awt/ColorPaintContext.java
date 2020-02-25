6e2917e4372c06928e9fe4fe89ad1143bbfbcb76
BobHanson <hansonr@stolaf.edu>
2020-02-25 00:08:02 -0600

    adds Color paint context

--- sources/net.sf.j2s.java.core/src/java/awt/ColorPaintContext.java
+++ sources/net.sf.j2s.java.core/src/java/awt/ColorPaintContext.java
@@ -31,10 +31,15 @@
 package java.awt;
 
 import java.awt.image.ColorModel;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.util.Arrays;
+
+import sun.awt.image.IntegerComponentRaster;
 
 class ColorPaintContext implements PaintContext {
     int color;
-//    WritableRaster savedTile;
+    WritableRaster savedTile;
 
     protected ColorPaintContext(int color, ColorModel cm) {
         this.color = color;
@@ -65,20 +70,21 @@ class ColorPaintContext implements PaintContext {
         return ColorModel.getRGBdefault();
     }
 
-//    public synchronized Raster getRaster(int x, int y, int w, int h) {
-//        WritableRaster t = savedTile;
-//
-//        if (t == null || w > t.getWidth() || h > t.getHeight()) {
-//            t = getColorModel().createCompatibleWritableRaster(w, h);
-//            IntegerComponentRaster icr = (IntegerComponentRaster) t;
-//            Arrays.fill(icr.getDataStorage(), color);
-//            // Note - markDirty is probably unnecessary since icr is brand new
-//            icr.markDirty();
-//            if (w <= 64 && h <= 64) {
-//                savedTile = t;
-//            }
-//        }
-//
-//        return t;
-//    }
+    @Override
+	public synchronized Raster getRaster(int x, int y, int w, int h) {
+        WritableRaster t = savedTile;
+
+        if (t == null || w > t.getWidth() || h > t.getHeight()) {
+            t = getColorModel().createCompatibleWritableRaster(w, h);
+            IntegerComponentRaster icr = (IntegerComponentRaster) t;
+            Arrays.fill(icr.getDataStorage(), color);
+            // Note - markDirty is probably unnecessary since icr is brand new
+            icr.markDirty();
+            if (w <= 64 && h <= 64) {
+                savedTile = t;
+            }
+        }
+
+        return t;
+    }
 }
