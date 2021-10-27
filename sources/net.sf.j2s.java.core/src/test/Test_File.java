83f1cdc28ec056537aa2f8abe5bde528cfadf304
hansonr <hansonr@stolaf.edu>
2021-10-27 04:43:44 -0500

    OC fix for .append and file and SwingJS
    (only used in Test_File)

--- sources/net.sf.j2s.java.core/src/test/Test_File.java
+++ sources/net.sf.j2s.java.core/src/test/Test_File.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
