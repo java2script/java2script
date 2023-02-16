68408a9f47283c90e038ba8d1ff1f19ced326da2
hansonr <hansonr@stolaf.edu>
2023-02-16 10:31:26 -0600

    getScript issue; will revert

--- sources/net.sf.j2s.java.core/srcjs/js/j2sApplet.js
+++ sources/net.sf.j2s.java.core/srcjs/js/j2sApplet.js
@@ -313,11 +313,22 @@ window.J2S = J2S = (function() {
 	}
 
 	J2S.$getScriptAsync = function(file, whenDone) {
+		// bad news: if the file does not exist, this fails to call any error. apparently fixed in JQuery 2.0
+		
 		if (J2S._nozcore) {
 			file = file.replace(/\.z\.js/,".js");
 		}
-		return $.getScript(file, whenDone);
+		return J2S.$ajax({
+			url: file,
+			type: "GET",
+			dataType: "script",
+			cache: "NO",
+			statusCode: { 404: function() {alert("!")}},
+			success: whenDone,
+			error: whenDone
+		});
 	}
+
 
 	var fixProtocol = function(url) {
 		if (!J2S._isFile && url.indexOf("file://") >= 0)
